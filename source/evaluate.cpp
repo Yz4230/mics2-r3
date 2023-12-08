1e640eec815e596cc5352d2790b3a6db40ef755c
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2023-12-08 18:28:40 +0900

    Improve evaluation function in evaluate.cpp

--- source/evaluate.cpp
+++ source/evaluate.cpp
@@ -43,7 +43,24 @@ Value evaluate(const Position &pos) {
   auto score = VALUE_ZERO;
 
   // 局面の評価
-  for (Square sq : SQ) score += PieceValue[pos.piece_on(sq)];
+  for (Square sq : SQ) {
+    // より相手陣に進んでいる駒ほど評価値を高くする
+    const auto pc = pos.piece_on(sq);
+    if (pc == NO_PIECE) continue;
+
+    const auto c = color_of(pc);
+    double r = (double)rank_of(sq);
+    if (c == BLACK) {
+      // 先手から見た段数に変換
+      r = RANK_5 - r;
+    }
+    r = (r * 0.05) + 1;
+
+    if (pc == B_KING || pc == W_KING) {
+      r = 1;
+    }
+    score += PieceValue[pos.piece_on(sq)] * r;
+  }
 
   // 手駒の評価
   for (Color c : COLOR) {
