4a0fb781f4728eddb626235e91275bea8ab535f1
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2023-12-13 15:21:22 +0900

    set quantized mode automatically

--- source/main.cpp
+++ source/main.cpp
@@ -11,8 +11,26 @@ int main(int argc, char *argv[]) {
   // --- コマンドライン引数の解析
   Args::parse(argc, argv);
 
+  // --- QNNPACKの初期化
+  bool use_quantized = false;
+  for (auto e : at::globalContext().supportedQEngines()) {
+    if (e == c10::QEngine::QNNPACK || e == c10::QEngine::X86) {
+      use_quantized = true;
+      at::globalContext().setQEngine(e);
+      break;
+    }
+  }
+
   // --- ニューラルネットワークの読み込み
-  torch::jit::script::Module model = torch::jit::load("./model-kp.pt");
+  torch::jit::script::Module model;
+  if (use_quantized) {
+    model = torch::jit::load("./model-kp-q8.pt");
+    printf("[info] Set quantized mode\n");
+  } else {
+    model = torch::jit::load("./model-kp.pt");
+    printf("[info] Set float mode\n");
+  }
+
   model.eval();
   Network::model = &model;
   printf("[info] Successfully loaded model\n");
